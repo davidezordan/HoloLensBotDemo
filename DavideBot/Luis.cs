b549444008e66cc4e009666335860c6ab628b67c
Davide Zordan <davide.zordan@microsoft.com>
2017-05-10 12:57:52 +0100

    Adding Demo

---
+++ DavideBot/Luis.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DavideBot
+{
+    public class LUISDavideBot
+    {
+        public static async Task<DavideBotLUIS> ParseUserInput(string strInput)
+        {
+            try
+            {
+                string strRet = string.Empty;
+                string strEscaped = Uri.EscapeDataString(strInput);
+
+                using (var client = new HttpClient())
+                {
+                    string uri = "<LUIS url here>" + strEscaped;
+                    HttpResponseMessage msg = await client.GetAsync(uri);
+
+                    if (msg.IsSuccessStatusCode)
+                    {
+                        var jsonResponse = await msg.Content.ReadAsStringAsync();
+                        var _Data = JsonConvert.DeserializeObject<DavideBotLUIS>(jsonResponse);
+                        return _Data;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+
+            return null;
+        }
+    }
+
+
+    public class DavideBotLUIS
+    {
+        public string query { get; set; }
+        public Topscoringintent topScoringIntent { get; set; }
+        public Intent[] intents { get; set; }
+        public Entity[] entities { get; set; }
+        public Dialog dialog { get; set; }
+    }
+
+    public class Topscoringintent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+        public Action[] actions { get; set; }
+    }
+
+    public class Action
+    {
+        public bool triggered { get; set; }
+        public string name { get; set; }
+        public object[] parameters { get; set; }
+    }
+
+    public class Dialog
+    {
+        public string contextId { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+        public Action1[] actions { get; set; }
+    }
+
+    public class Action1
+    {
+        public bool triggered { get; set; }
+        public string name { get; set; }
+        public object[] parameters { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public float score { get; set; }
+    }
+}
---
+++ HoloLensBotDemo/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Bot.Connector.DirectLine;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HoloLensBotDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            TextStatus.Text = "Listening....";
+            
+            // Create an instance of SpeechRecognizer.
+            var speechRecognizer = new Windows.Media.SpeechRecognition.SpeechRecognizer();
+
+            // Compile the dictation grammar by default.
+            await speechRecognizer.CompileConstraintsAsync();
+
+            // Start recognition.
+            Windows.Media.SpeechRecognition.SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeWithUIAsync();
+
+            TextCommand.Text = speechRecognitionResult.Text;
+
+            await SendToBot(TextCommand.Text);
+        }
+
+        private async Task SendToBot(string inputText)
+        {
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                var directLine = new DirectLineClient("<Direct Line Secret Key here>");
+                var conversation = await directLine.Conversations.StartConversationWithHttpMessagesAsync();
+                var convId = conversation.Body.ConversationId;
+                var httpMessages = await directLine.Conversations.GetActivitiesWithHttpMessagesAsync(convId);
+
+                var command = inputText;
+                TextStatus.Text = "Sending text to Bot....";
+                var postMessage = await directLine.Conversations.PostActivityWithHttpMessagesAsync(convId,
+                    new Activity()
+                    {
+                        Type = "message",
+                        From = new ChannelAccount()
+                        {
+                            Id = "Davide"
+                        },
+                        Text = command
+                    });
+
+                var result = await directLine.Conversations.GetActivitiesAsync(convId);
+                if (result.Activities.Count > 0)
+                {
+                    var firstOrDefault = result
+                        .Activities
+                        .FirstOrDefault(a => a.From != null
+                        && a.From.Name != null
+                        && a.From.Name.Equals("Davide Personal Bot"));
+                    if (firstOrDefault != null)
+                    {
+                        TextOutputBot.Text = "Bot response: " + firstOrDefault.Text;
+                        TextStatus.Text = string.Empty;
+                    }
+                }
+            }
+        }
+    }
+}
