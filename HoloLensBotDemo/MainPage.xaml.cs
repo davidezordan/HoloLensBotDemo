aa77ae2be31c7f7026927dc93d3c9c45d4ba732f
Davide Zordan <davide.zordan@microsoft.com>
2017-07-26 12:48:16 +0100

    Adding Text to Speech

--- HoloLensBotDemo/MainPage.xaml.cs
+++ HoloLensBotDemo/MainPage.xaml.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -13,12 +15,23 @@ namespace HoloLensBotDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SpeechSynthesizer synthesizer;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            synthesizer = new SpeechSynthesizer();
+
+            media.MediaEnded += Media_MediaEnded;
         }
 
-        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void Media_MediaEnded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            StartRecognitionButton_Click(null, null);
+        }
+
+        private async void StartRecognitionButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             TextStatus.Text = "Listening....";
             
@@ -40,7 +53,7 @@ namespace HoloLensBotDemo
         {
             if (!string.IsNullOrEmpty(inputText))
             {
-                var directLine = new DirectLineClient("<Direct Line Secret Key here>");
+                var directLine = new DirectLineClient("<Direct Line key here>");
                 var conversation = await directLine.Conversations.StartConversationWithHttpMessagesAsync();
                 var convId = conversation.Body.ConversationId;
                 var httpMessages = await directLine.Conversations.GetActivitiesWithHttpMessagesAsync(convId);
@@ -61,16 +74,50 @@ namespace HoloLensBotDemo
                 var result = await directLine.Conversations.GetActivitiesAsync(convId);
                 if (result.Activities.Count > 0)
                 {
-                    var firstOrDefault = result
+                    var botResponse = result
                         .Activities
-                        .FirstOrDefault(a => a.From != null
-                        && a.From.Name != null
-                        && a.From.Name.Equals("Davide Personal Bot"));
-                    if (firstOrDefault != null)
+                        .LastOrDefault(a => a.From != null && a.From.Name != null && a.From.Name.Equals("Davide Personal Bot"));
+                    if (botResponse != null && !string.IsNullOrEmpty(botResponse.Text))
                     {
-                        TextOutputBot.Text = "Bot response: " + firstOrDefault.Text;
+                        var response = botResponse.Text;
+
+                        TextOutputBot.Text = "Bot response: " + response;
                         TextStatus.Text = string.Empty;
+
+                        Speech(response);
                     }
+                }
+            }
+        }
+
+        private async void Speech(string text)
+        {
+            if (media.CurrentState == MediaElementState.Playing)
+            {
+                media.Stop();
+            }
+            else
+            {
+                try
+                {
+                    // Create a stream from the text. This will be played using a media element.
+                    SpeechSynthesisStream synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(text);
+
+                    // Set the source and start playing the synthesized audio stream.
+                    media.AutoPlay = true;
+                    media.SetSource(synthesisStream, synthesisStream.ContentType);
+                    media.Play();
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    var messageDialog = new Windows.UI.Popups.MessageDialog("Media player components unavailable");
+                    await messageDialog.ShowAsync();
+                }
+                catch (Exception)
+                {
+                    media.AutoPlay = false;
+                    var messageDialog = new Windows.UI.Popups.MessageDialog("Unable to synthesize text");
+                    await messageDialog.ShowAsync();
                 }
             }
         }
